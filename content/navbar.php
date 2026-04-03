714ec59be628f89ba82fb193855832b98ca0d46c
jenpyle <jennypyle440@gmail.com>
2026-04-03 17:26:53 -0600

    Fix style sheet, direction switch keeps current page, add dynamic header

--- content/navbar.php
+++ content/navbar.php
@@ -1,48 +1,41 @@
 <!-- https://cs.colostate.edu:4444/~C830350809/eastwest/content/navbar.php -->
 <!-- in the navbar you should be using the GET and POST to change direction variable for the home page -->
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Navigation Bar</title>
-</head>
 <header>
-    <h1> Navigation Bar </h1>
+    <h2>Navigation Bar</h2>
 </header>
-<main>
 
-    <ul>
+<ul class="navigation-list">
 
+    <?php if ($direction === "west") { ?>
         <li>
             <form method="GET" action="index.php">
-                <input type="hidden" name="direction" value="east"></input><br>
-                <button type="submit">East</button>
+                <input type="hidden" name="direction" value="east">
+                <input type="hidden" name="page" value="<?php echo $page; ?>">
+                <button type="submit">Switch to East</button>
             </form>
         </li>
+    <?php } else { ?>
         <li>
             <form method="GET" action="index.php">
-                <input type="hidden" name="direction" value="west"></input><br>
-                <button type="submit">West</button>
+                <input type="hidden" name="direction" value="west">
+                <input type="hidden" name="page" value="<?php echo $page; ?>">
+                <button type="submit">Switch to West</button>
             </form>
         </li>
+    <?php } ?>
 
-        <li>
-            <form method="POST" action="index.php?direction=<?php echo $direction ?>">
-                <!-- page is the variable that's going to change and set which page to change -->
-                <input type="hidden" name="page" value="home"></input><br>
-                <button type="submit">Home Page</button>
-            </form>
-        </li>
-        <li>
-            <form method="POST" action="index.php?direction=<?php echo $direction ?>">
-                <input type="hidden" name="page" value="about"></input><br>
-                <button type="submit">About Page</button>
-            </form>
-        </li>
+    <li>
+        <form method="POST" action="index.php?direction=<?php echo $direction; ?>">
+            <input type="hidden" name="page" value="home">
+            <button type="submit">Home Page</button>
+        </form>
+    </li>
 
-    </ul>
+    <li>
+        <form method="POST" action="index.php?direction=<?php echo $direction; ?>">
+            <input type="hidden" name="page" value="about">
+            <button type="submit">About Page</button>
+        </form>
+    </li>
 
-</main>
-
-</html>
+</ul>
--- index.php
+++ index.php
@@ -1,53 +1,51 @@
 <!-- https://cs.colostate.edu:4444/~C830350809/eastwest/index.php -->
 <!-- in the index is supposed to be logic of if direction variable then change the style to east or west -->
-<!DOCTYPE html>
-<html lang="en">
-
 <?php
-// Handle direction (GET)
+//Switch direction GET
 if (isset($_GET["direction"])) {
     $direction = $_GET["direction"];
 } else {
-    $direction = "east";
+    $direction = "east"; // default
 }
 
-// Only allow east or west
-if ($direction !== "east" && $direction !== "west") {
-    $direction = "east";
-}
 
-// Handle page (POST)
+//Switch page (POST)
 if (isset($_POST["page"])) {
     $page = $_POST["page"];
+} elseif (isset($_GET["page"])) {
+    $page = $_GET["page"];
 } else {
     $page = "home";
 }
 
-// Only allow valid page names
 if ($page !== "home" && $page !== "about") {
     $page = "home";
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="author" content="Jennifer Pyle" />
-    <meta name="description" content="Portfolio site for Jennifer Pyle" />
-    <meta name="keywords" content="HTML, CSS, JavaScript, PHP" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Jennifer Pyle - Homepage1</title>
-    <link rel="stylesheet" type="text/css" href="content/<?php echo $direction; ?>.css">
+    <meta charset="UTF-8">
+    <meta name="author" content="Jennifer Pyle">
+    <meta name="description" content="Portfolio site for Jennifer Pyle">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jennifer Pyle - Homepage</title>
+    <link rel="stylesheet" href="content/<?php echo $direction; ?>.css">
 </head>
 
 <body>
+
+    <!-- Header (uses $direction and $page) -->
     <?php include 'content/header.php'; ?>
     <?php include 'content/navbar.php'; ?>
-
     <main>
         <?php include 'content/' . $page . '.php'; ?>
     </main>
-
     <?php include 'content/footer.php'; ?>
+
 </body>
 
 </html>
